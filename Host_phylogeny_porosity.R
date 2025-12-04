0092de9898b724278f6803c64b7a63e6be90b15a
Ollie Spacey <ogspacey16@gmail.com>
2025-12-04 11:54:10 +0000

    Created script outline to plot phylogeny of Viscum host genera and their porosity

---
+++ Host_phylogeny_porosity.R
@@ -0,0 +1,41 @@
+### Host phylogeny and porosity
+### Oliver G. Spacey
+### 2025.12.04
+
+# Code to plot a phylogeny of hosts of Viscum album subsp. album across angiosperms
+# And their traits, e.g., wood porosity
+
+
+# Pre-amble ---------------------------------------------------------------
+# Clear environment
+rm(list = ls())
+
+# Load required packages
+
+
+# Load data ---------------------------------------------------------------
+# Load porosity dataset
+
+
+# Load hosts of Viscum album subsp album
+
+
+# Load preference data - LATER
+
+
+# Wrangle data ------------------------------------------------------------
+# Remove duplicates/unspecified
+
+# Calculate porosity score for each species
+
+# Calculate for each genus - run through where not in agreement across genus? or calculate mean across genus?
+
+# Phylogenetic analysis ---------------------------------------------------
+# Calculate phylogenetic inertia for porosity
+
+# Calculate phylogenetic inertia for hosts
+
+# Calculate co-inertia?
+
+# Calculate phylogenetically controlled relationship between porosity score and host (phylog-controlled logistic regression)
+
